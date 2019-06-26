0309b153c67386f064d86c31095d6f8d4c39c2f4
Mauricio Costa <Mauricio_Costa@dell.com>
2019-06-26 12:43:02 -0300

    NGRouting setup + FontAwesome

--- wwwroot/scripts/directline/app.js
+++ wwwroot/scripts/directline/app.js
@@ -1,4 +1,33 @@
-﻿angular.module("directlineApp", ["ngMaterial","ngMessages"])
-    .config(function ($mdThemingProvider) {
+﻿angular.module("directlineApp", ["ngMaterial", "ngMessages","ngAnimate","ngMaterial", "ngRoute"])
+    .config(function ($mdThemingProvider, $mdIconProvider, $routeProvider) {
         $mdThemingProvider.theme("default");
+        $mdIconProvider          
+            .fontSet('fa', 'fontawesome');
+        $routeProvider
+            .when('/', {
+
+            })
+            .when('/tokenGen', {
+
+            })
+            .when('/swagger', {
+                redirectTo: function (routeParams) {
+                    return $location('/swagger');
+                }
+            })
+            .when('/about', {
+                template:"<h1>About</h1>"
+            });
+       
+    })
+   
+    .controller("AppController", function ($scope, $mdSidenav) {
+        $scope.toggleMenu = function toggleMenu() {
+            $mdSidenav('left').toggle();
+        };
+        $scope.pages = [
+            { icon: "fas fa-key", name: "Generate Token", url:"tokenGen"},
+            { icon: "fas fa-link", name: "API Primitives", url: "swagger" },
+            { icon: "fas fa-info", name: "About", url:"about"}
+        ];
     });
